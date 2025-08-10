05affa2381d025dabde4efc4bc48997d97769104
Zeynep Hanife Çelikoğlu <zeynephcelikoglu@icloud.com>
2025-08-10 13:58:13 +0300

    feat: add base navigation structure with tabs and movie details screen

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+
+const _layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          headerShown: false,
+        }}
+      />
+    </Tabs>
+  )
+}
+
+export default _layout
