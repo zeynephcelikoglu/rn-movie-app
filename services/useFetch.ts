f65f744ad9cd3136f4dddfc45eb86943933d245d
Zeynep Hanife Çelikoğlu <zeynephcelikoglu@icloud.com>
2025-08-18 20:40:17 +0300

    feat(hooks): add useFetch hook for movie app

---
+++ services/useFetch.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const result = await fetchFunction();
+      setData(result);
+    } catch (err) {
+      // @ts-ignore
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setError(null);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, loading, error, refetch: fetchData, reset };
+};
+
+export default useFetch;
