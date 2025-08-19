a6df7222c7f7189a6debd5a1431a35ebf2daac91
Zeynep Hanife Çelikoğlu <zeynephcelikoglu@icloud.com>
2025-08-19 23:53:39 +0300

    wip(movies): fetching and displaying movie list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,20 @@
-import { View, Image, ScrollView } from "react-native";
-import { images } from "@/constants/images";
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
-import  SearchBar  from "@/components/SearchBar";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
 
   const router = useRouter();
+
+  const {data: movies, 
+    loading: moviesLoading, 
+    error: moviesError} = useFetch(() => fetchMovies({ 
+      query: '' 
+    }));
 
   return (
     <View className="flex-1 bg-primary">
@@ -25,14 +33,38 @@ export default function Index() {
         <Image
           source={icons.logo}
           className="w-12 h-10 mt-20 mb-5 mx-auto"/>
-        
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => router.push('/search')}
-            placeholder="Search for a movie"
-          />
-        </View>
 
+          {moviesLoading ? (
+            <ActivityIndicator
+              size="large"
+              color="#0000ff"
+              className="mt-10 self-center"
+              />
+          ) : moviesError ? (
+            <Text>Error: {moviesError?.message}</Text>
+          ) : (
+            <View className="flex-1 mt-5">
+              <SearchBar
+                onPress={() => router.push('/search')}
+                placeholder="Search for a movie"
+              />
+              <>
+                <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+
+                <FlatList
+                  data={movies}
+                  renderItem={({item})=> (
+                    <Text className="text-white text-sm">{item.title}</Text>
+                  )}
+                  keyExtractor={(item) => item.id.toString()}
+                  numColumns={3}
+                  columnWrapperStyle={{ justifyContent: 'flex-start', gap: 20, paddingRight: 5, marginBottom: 10 }}
+                  className="mt-2 b-32"
+                  scrollEnabled={false}
+                  />
+              </>
+            </View>
+          )}
       </ScrollView>
     </View>
   );
--- services/api.ts
+++ services/api.ts
@@ -4,14 +4,16 @@ export const TMDB_CONFIG = {
     headers: {
         accept: 'application/json',
         Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
-
     }
 }
 
-export const fetchPopularMovies = async ({ query }: { query: string }) => {
+export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query
-    ? '${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}'
-    : '${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc';
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+  console.log('FETCH URL ===>', endpoint);
+  console.log('AUTH HEAD ===>', TMDB_CONFIG.headers.Authorization?.slice(0, 25), '...');
 
   const response = await fetch(endpoint, {
     method: 'GET',
@@ -19,10 +21,9 @@ export const fetchPopularMovies = async ({ query }: { query: string }) => {
   });
 
   if (!response.ok) {
-    // @ts-ignore
-    throw new Error('Failed to fetch movies', response.statusText);
+    throw new Error(`Failed to fetch movies (${response.status})`);
   }
-
   const data = await response.json();
   return data.results;
 };
+
